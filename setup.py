7ef4e86f9c9feaa93cc3854fb0a097bd2b765382
sarojaerabelli <saroja.erabelli@gmail.com>
2020-07-23 07:59:56 -0400

    Changed setup file.

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="util", packages=find_packages())
-setup(name="bfv", packages=find_packages())
-setup(name="ckks", packages=find_packages())
-setup(name="tests", packages=find_packages())
+setup(name = "py-fhe", packages = find_packages())
